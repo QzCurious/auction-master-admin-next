c029379b46cd03b314266e13c9ef575c957e8671
Qz <qzcurious@gmail.com>
2024-05-28 17:02:43 +0800

    fix middleware logout

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,18 +10,27 @@ export async function middleware(request: NextRequest) {
     return Response.redirect(new URL(`/auth/sign-in?goto=${request.nextUrl.pathname}`, request.url));
   }
 
-  // refresh token and set cookie
-  const response = NextResponse.next();
-  const { token: newToken } = await refreshTokenIfExpired();
-  if (newToken) {
-    response.cookies.set('token', newToken, {
-      expires: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // 14 days
-      httpOnly: true,
-      sameSite: 'strict',
-      secure: process.env.NODE_ENV === 'production',
-    });
+  if (request.nextUrl.pathname.startsWith('/auth/sign-in')) {
+    return NextResponse.next();
   }
 
+  // refresh token and set cookie
+  const response = NextResponse.next();
+  try {
+    const { token: newToken } = await refreshTokenIfExpired();
+    if (newToken) {
+      response.cookies.set('token', newToken, {
+        expires: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // 14 days
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+      });
+    }
+  } catch (e) {
+    if (e instanceof Error && e.message === 'NEXT_REDIRECT') {
+      return NextResponse.redirect(new URL('/auth/sign-in', request.url));
+    }
+  }
   return response;
 }
 
