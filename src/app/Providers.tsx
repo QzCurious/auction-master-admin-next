9ef54da04debd266d9cb1169397fb89f7ced5645
Qz <qzcurious@gmail.com>
2024-06-16 20:54:55 +0800

    clean up

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,20 +1,17 @@
 'use client';
 
-import * as React from 'react';
 import { SnackbarProvider } from 'notistack';
+import * as React from 'react';
 
-import { UserProvider } from '@/contexts/user-context';
 import { LocalizationProvider } from '@/components/core/localization-provider';
 import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <LocalizationProvider>
-      <UserProvider>
-        <ThemeProvider>
-          <SnackbarProvider>{children}</SnackbarProvider>
-        </ThemeProvider>
-      </UserProvider>
+      <ThemeProvider>
+        <SnackbarProvider>{children}</SnackbarProvider>
+      </ThemeProvider>
     </LocalizationProvider>
   );
 }
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import type { Metadata } from 'next';
+import RouterLink from 'next/link';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import { config } from '@/config';
-import { Layout } from '@/components/auth/layout';
+import { paths } from '@/paths';
+import { DynamicLogo } from '@/components/core/logo';
 
 import { SignInForm } from './Form';
 
@@ -10,8 +15,58 @@ export const metadata = { title: `Sign in | Auth | ${config.site.name}` } satisf
 
 export default function Page(): React.JSX.Element {
   return (
-    <Layout>
-      <SignInForm />
-    </Layout>
+    <Box
+      sx={{
+        display: { xs: 'flex', lg: 'grid' },
+        flexDirection: 'column',
+        gridTemplateColumns: '1fr 1fr',
+        minHeight: '100%',
+      }}
+    >
+      <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
+        <Box sx={{ p: 3 }}>
+          <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
+            <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
+          </Box>
+        </Box>
+        <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
+          <Box sx={{ maxWidth: '450px', width: '100%' }}>
+            <SignInForm />
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          alignItems: 'center',
+          background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
+          color: 'var(--mui-palette-common-white)',
+          display: { xs: 'none', lg: 'flex' },
+          justifyContent: 'center',
+          p: 3,
+        }}
+      >
+        <Stack spacing={3}>
+          <Stack spacing={1}>
+            <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
+              Welcome to{' '}
+              <Box component="span" sx={{ color: '#15b79e' }}>
+                Devias Kit
+              </Box>
+            </Typography>
+            <Typography align="center" variant="subtitle1">
+              A professional template that comes with ready-to-use MUI components.
+            </Typography>
+          </Stack>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Box
+              component="img"
+              alt="Widgets"
+              src="/assets/auth-widgets.png"
+              sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
+            />
+          </Box>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
