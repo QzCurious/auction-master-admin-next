f6e70dfef8f58098f1d4865501acf11c3f77d5cc
Qz <qzcurious@gmail.com>
2024-08-05 23:39:17 +0800

    bypass sentry for development because it slow down dev server significantly

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,50 @@
-import { withSentryConfig } from '@sentry/nextjs';
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
 
-/** @type {import('next').NextConfig} */
-const config = {
-  output: 'standalone',
-};
+// eslint-disable-next-line func-names
+export default async function (phase, { defaultConfig }) {
+  /** @type {import('next').NextConfig} */
+  const config = {
+    output: 'standalone',
+  };
 
-export default withSentryConfig(config, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return config;
+  }
 
-  org: 'auction-master',
-  project: 'auction-master-admin',
+  const { withSentryConfig } = await import('@sentry/nextjs');
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  return withSentryConfig(config, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    org: 'auction-master',
+    project: 'auction-master-admin',
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  });
+}
