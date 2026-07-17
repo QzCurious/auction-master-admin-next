37d9c53938ab2fb24b2b4390912f287684b6d2df
Qz <38932402+QzCurious@users.noreply.github.com>
2026-07-17 14:20:19 +0800

    Replace Consul with direct S3 environment config

--- src/connect.ts
+++ src/connect.ts
@@ -1,100 +1,35 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import dotenv from 'dotenv';
 
 import { lazySingleton } from './helper/singleton';
 
-if (!process.env.CONSUL_URL) {
-  throw new Error('CONSUL_URL is not set');
+function requireEnv(name: string): string {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`${name} is not set`);
+  }
+
+  return value;
 }
 
-export const getS3Kv = lazySingleton(async () => {
-  const url = `${process.env.CONSUL_URL}/v1/kv/storage/aws-s3/auction-master`;
-  const res = await fetch(url);
+export const getS3Kv = lazySingleton(async () => ({
+  access_key_id: requireEnv('S3_ACCESS_KEY_ID'),
+  secret_access_key: requireEnv('S3_SECRET_ACCESS_KEY'),
+  region: requireEnv('S3_REGION'),
+  endpoint_version: process.env.S3_ENDPOINT_VERSION ?? '',
+  endpoint: requireEnv('S3_ENDPOINT'),
+  public_url: requireEnv('S3_PUBLIC_URL').replace(/\/$/, ''),
+  acl: process.env.S3_ACL ?? '',
+  bucket: requireEnv('S3_BUCKET'),
+}));
 
-  if (!res.ok) {
-    throw new Error('Cannot get consol s3 data');
-  }
-
-  const [x] = (await res.json()) as Array<{
-    LockIndex: number;
-    Key: string;
-    Flags: number;
-    Value: string;
-    CreateIndex: number;
-    ModifyIndex: number;
-  }>;
-
-  const kv = dotenv.parse(Buffer.from(x.Value, 'base64')) as {
-    access_key_id: string;
-    secret_access_key: string;
-    region: string;
-    endpoint_version: string;
-    endpoint: string;
-    public_url: string;
-    acl: string;
-    bucket: string;
-  };
-
-  return kv;
-});
-
-export const getMysqlKv = lazySingleton(async () => {
-  const url = `${process.env.CONSUL_URL}/v1/kv/storage/mysql/master/auction-master`;
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error('Cannot get consol mysql data');
-  }
-
-  const [x] = (await res.json()) as Array<{
-    LockIndex: number;
-    Key: string;
-    Flags: number;
-    Value: string;
-    CreateIndex: number;
-    ModifyIndex: number;
-  }>;
-
-  const kv = dotenv.parse(Buffer.from(x.Value, 'base64')) as {
-    host: string;
-    account: string;
-    password: string;
-    dbname: string;
-    // tls: string;
-    // max_open_conns: string;
-    // max_idle_conns: string;
-    // max_conn_lifetime: string;
-  };
-
-  return kv;
-});
-
-export const getSystemKv = lazySingleton(async () => {
-  const url = `${process.env.CONSUL_URL}/v1/kv/system`;
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error('Cannot get consol system data');
-  }
-
-  const [x] = (await res.json()) as Array<{
-    LockIndex: number;
-    Key: string;
-    Flags: number;
-    Value: string;
-    CreateIndex: number;
-    ModifyIndex: number;
-  }>;
-
-  const kv = dotenv.parse(Buffer.from(x.Value, 'base64')) as {
-    site: string;
-  };
-
-  return kv;
-});
+export const getSystemKv = lazySingleton(async () => ({
+  site: requireEnv('S3_SITE').replace(/^\/+|\/+$/g, ''),
+}));
 
 export const getS3Client = lazySingleton(async () => {
   const s3Kv = await getS3Kv();
+
   return new S3Client({
     region: s3Kv.region,
     endpoint: s3Kv.endpoint,
